ba5da932cba2df720b02587b6f41b63b885f85a4
SEFSIS <sofiagryn@icloud.com>
2023-10-23 01:08:53 +0300

    homework2

--- app.js
+++ app.js
@@ -1,109 +1,101 @@
-//EVENTS
-// const Events = require('node:events');
-//
-// const eventEmitter = new Events();
-//
-// eventEmitter.on('click', () => { //декларуємо івент
-//     console.log('click click click');
-// })
-//
-// eventEmitter.emit('click');//виклик івенту
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-//
-// eventEmitter.once('click once', () => {
-//     console.log('click once');
-// })
-//
-// console.log(eventEmitter.eventNames());
-//
-// eventEmitter.emit('click once');//click once
-// eventEmitter.emit('click once');//event does not exist
-//
-// console.log(eventEmitter.eventNames());
-
-//STREAM
-// const fs = require('node:fs');
-// const path = require('node:path');
-//
-//
-// const readStream = fs.createReadStream(path.join(__dirname, 'folder', 'text3.txt'),{highWaterMark: 128 * 1024});
-// const writeStream = fs.createWriteStream(path.join(__dirname, 'folder', 'text2.txt'));
-//
-// readStream.on('data', (chunk) => {
-//     console.log(chunk);
-//     writeStream.write(chunk);
-// });
-//
-// readStream.pipe(writeStream);
-//
-// readStream.on('error', () => {
-//     console.log('error happened');
-//     readStream.destroy();
-//     writeStream.end('ERROR HAPPENED');
-// });
-
 const express = require('express');
+const fsService = require('./fs.service');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const users = [
-    { id: 1, name: "Іван", email: "ivan@example.com" },
-    { id: 2, name: "Марія", email: "maria@example.com" },
-    { id: 3, name: "Петро", email: "petro@example.com" },
-    { id: 4, name: "Анна", email: "anna@example.com" },
-    { id: 5, name: "Олександр", email: "oleksandr@example.com" },
-    { id: 6, name: "Юлія", email: "yulia@example.com" },
-    { id: 7, name: "Василь", email: "vasyl@example.com" },
-    { id: 8, name: "Оксана", email: "oksana@example.com" },
-    { id: 9, name: "Сергій", email: "serhiy@example.com" },
-    { id: 10, name: "Наталія", email: "natalia@example.com" }
-];
-
-app.get('/users', (req,res) => {
-    res.json({
-        data:users,
-    })
+app.get('/users', async (req, res) => {
+    const users = await fsService.reader();
+    res.json(users);
 })
 
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params;
+app.post('/users', async (req, res) => {
+    try {
+        const {name, email} = req.body;
 
-    res.json({
-        data: users[+id - 1],
-    })
+        if (!name || name.length < 3) {
+            throw new Error('Wrong name');
+        }
+        if (!email || !email.includes("@")) {
+            throw new Error('Wrong email');
+        }
+
+        const users = await fsService.reader();
+
+        const lastId = users[users.length - 1].id;
+        const newUser = {name, email, id: lastId + 1}
+        users.push(newUser);
+        await fsService.writer(users);
+
+        res.status(201).json(newUser);
+    } catch (e) {
+        return res.status(400).json(e.message);
+    }
+
 })
 
-app.post('/users', (req, res) => {
-    users.push(req.body);
+app.get('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const users = await fsService.reader();
+        const user = users.find((user) => user.id === Number(id));
+        if(!user){
+            throw new Error('User not found')
+        }
+        res.json(user);
+    }catch (e) {
+       res.status(404).json(e.message);
+    }
+});
 
-    res.status(201).json({
-        message:"User created",
-    });
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const users = await fsService.reader();
+        const index = users.findIndex((user) => user.id === Number(id));
+        if (index === -1){
+            throw new Error("User not found");
+        }
+        users.splice(index, 1);
+
+        await fsService.writer(users);
+
+        res.sendStatus(204);
+    }catch (e) {
+        res.status(404).json(e.message);
+    }
 })
 
-app.delete('/users/:id', (req, res) => {
-    const {id} = req.params;
+app.put('/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {name,email} = req.body;
 
-    users.splice(+id - 1, 1);
+        if (!name || name.length < 3) {
+            throw new Error('Wrong name');
+        }
+        if (!email || !email.includes("@")) {
+            throw new Error('Wrong email');
+        }
 
-    res.sendStatus(204);
-})
+        const users = await fsService.reader();
+        const user = users.find((user) => user.id === Number(id));
+        if (!user){
+            throw new Error("User not found");
+        }
+        user.email = email;
+        user.name = name;
 
-app.put('/users/:id',(req,res)=>{
-    const {id} = req.params;
+        await fsService.writer(users);
 
-    users[id] = req.body;
-
-    res.json({
-        message: 'User updated'
-    })
-})
+        res.status(201).json(user);
+    }catch (e) {
+        res.status(404).json(e.message);
+    }
+});
 
 const PORT = 5005;
 
